41aa4f5f94f5883b5eb3e9bf955b530922821781
niyonx <kenkenysy@gmail.com>
2020-05-24 17:36:53 -0400

    Fixed deployment file

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ set -e
 # build
 npm run build
 
+git checkout gh-pages
+
 # navigate into the build output directory
 cd dist
 
@@ -16,10 +18,14 @@ git init
 git add -A
 git commit -m 'deploy'
 
+git remote add origin https://github.com/niyonx/eternity.git
+
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:niyonx/eternity.git master:gh-pages
+git push -f origin master:gh-pages
+
+git checkout master
 
 cd -
