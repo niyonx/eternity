84794eba6ffae92c836ab2cdd556ac411babfd91
Tianxiang Ying <896267775@qq.com>
2020-05-24 10:57:54 +0800

    Update the UI

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,189 +1,195 @@
 <template>
-  <div class="container">
-    <div class="calculator">
-      <div class="display">{{current || 0}}</div>
-      <div @click="clear" class="btn operator basic">C</div>
-      <div @click="sign" class="btn operator basic">+/-</div>
-      <div @click="percent" class="btn operator basic">%</div>
-      <div @click="divide" class="btn operator">/</div>
-      <div class="btn operator">deg</div>
-      <div @click="backspace" class="btn operator">&#x2190;</div>
-      <div @click="append(7)" class="btn">7</div>
-      <div @click="append(8)" class="btn">8</div>
-      <div @click="append(9)" class="btn">9</div>
-      <div @click="times" class="btn operator">*</div>
-      <div class="btn operator">e<sup>x</sup></div>
-      <div class="btn operator">sin</div>
-      <div @click="append(4)" class="btn">4</div>
-      <div @click="append(5)" class="btn">5</div>
-      <div @click="append(6)" class="btn">6</div>
-      <div @click="minus" class="btn operator">-</div>
-      <div class="btn operator">x<sup>y</sup></div>
-      <div class="btn operator">sinh</div>
-      <div @click="append(1)" class="btn">1</div>
-      <div @click="append(2)" class="btn">2</div>
-      <div @click="append(3)" class="btn">3</div>
-      <div @click="add" class="btn operator">+</div>
-      <div class="btn operator">ln</div>
-      <div class="btn operator">10<sup>x</sup></div>
-      <div @click="dot" class="btn">.</div>
-      <div @click="append(0)" class="zero btn">0</div>
-      <div @click="equal" class="btn operator equal">=</div>
-      <div @click="append('(')" class="btn operator">(</div>
-      <div @click="append(')')" class="btn operator">)</div>
+
+  <div class="calculator">
+    <div class="calculator-bar">
+      ETERNITY CALCULATOR
+    </div>
+    <div class="calculator-display">{{expression || 0}}</div>
+
+    <div class="calculator-keys">
+      <div class="row">
+        <button class="calculator-btn" @click="append('sin(')">sin</button>
+        <button class="calculator-btn" @click="append('sinh(')">sinh</button>
+        <button class="calculator-btn" @click="append('ln(')">ln</button>
+        <button class="calculator-btn" @click="append('10power(')">10<sup>x</sup></button>
+      </div>
+
+      <div class="row">
+        <button class="calculator-btn" @click="append('epower(')">e<sup>x</sup></button>
+        <button class="calculator-btn" @click="append('power(')">x<sup>y</sup></button>
+        <button class="calculator-btn" @click="append('e')">e</button>
+        <button class="calculator-btn" @click="append('π')">π</button>
+      </div>
+      <div class="row">
+        <button class="calculator-btn" @click="append('%')">%</button>
+        <button class="calculator-btn" @click="append('deg')">deg</button>
+        <button class="calculator-btn" @click="append('(')">(</button>
+        <button class="calculator-btn" @click="append(')')">)</button>
+      </div>
+      <div class="row">
+        <button class="calculator-btn" @click="append('*')">*</button>
+        <button class="calculator-btn" @click="append('/')">/</button>
+        <button class="calculator-btn" @click="backspace">←</button>
+        <button class="calculator-btn" @click="clear">C</button>
+      </div>
+      <div class="row">
+        <button class="calculator-btn num-btn" @click="append('7')">7</button>
+        <button class="calculator-btn num-btn" @click="append('8')">8</button>
+        <button class="calculator-btn num-btn" @click="append('9')">9</button>
+        <button class="calculator-btn" @click="append('-')">-</button>
+      </div>
+      <div class="row">
+        <button class="calculator-btn num-btn" @click="append('4')">4</button>
+        <button class="calculator-btn num-btn" @click="append('5')">5</button>
+        <button class="calculator-btn num-btn" @click="append('6')">6</button>
+        <button class="calculator-btn" @click="append('+')">+</button>
+      </div>
+      <div class="row">
+        <button class="calculator-btn num-btn " @click="append('1')">1</button>
+        <button class="calculator-btn num-btn " @click="append('2')">2</button>
+        <button class="calculator-btn num-btn " @click="append('3')">3</button>
+        <button class="calculator-btn eq-btn" @click="equal">=</button>
+      </div>
+      <div class="row">
+        <button class="calculator-btn num-btn" @click="append('-')">+/-</button>
+        <button class="calculator-btn num-btn" @click="append('0')">0</button>
+        <button class="calculator-btn num-btn" @click="append('.')">.</button>
+        <button class="calculator-btn notshow"></button>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 /* eslint-disable */
-export default {
-  name: 'Calculator',
-  data () {
-    return {
-      previous: null,
-      current: '',
-      operator: null,
-      operatorClicked: false
-    }
-  },
-  methods: {
-    clear () {
-      this.current = ''
-    },
-    sign () {
-      if (this.current !== '') {
-        this.current = this.current.charAt(0) === '-'
-          ? this.current.slice(1) : `-${this.current}`
+  export default {
+    name: 'Calculator',
+    data () {
+      return {
+        expression: ''
       }
     },
-    percent () {
-      if (this.current !== '') {
-        this.current = `${parseFloat(this.current) / 100}`
+    methods: {
+      clear () {
+        this.expression = ''
+      },
+      backspace () {
+        this.expression = this.expression.slice(0, -1)
+      },
+      append (e) {
+        this.expression += e
+      },
+      equal(){
+
       }
-    },
-    append (number) {
-      if (this.operatorClicked) {
-        this.current = ''
-        this.operatorClicked = false
-      }
-      this.current = `${this.current}${number}`
-    },
-    dot () {
-      if (this.current.indexOf('.') === -1) {
-        this.append('.')
-      }
-    },
-    setPrevious () {
-      this.previous = this.current
-      this.operatorClicked = true
-    },
-    divide () {
-      this.operator = (a, b) => Number(a / b)
-      this.setPrevious()
-    },
-    times () {
-      this.operator = (a, b) => Number(a * b)
-      this.setPrevious()
-    },
-    minus () {
-      this.operator = (a, b) => Number(b - a)
-      this.setPrevious()
-    },
-    add () {
-      this.operator = (a, b) => Number(a + b)
-      this.setPrevious()
-    },
-    equal () {
-      this.current = `${this.operator(
-        parseFloat(this.current),
-        parseFloat(this.previous)
-      )}`
-      this.previous = null
-    },
-    backspace () {
-      this.current = this.current.slice(0, -1)
+
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-@import url("https://fonts.googleapis.com/css?family=Poppins:300,500&display=swap");
+  /*@import url("https://fonts.googleapis.com/css?family=Poppins:300,500&display=swap");*/
 
-sup {
-  font-size:x-small;
-  vertical-align:super;
-}
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+  }
 
-.calculator {
-  display: grid;
-  grid-template-rows: repeat(6, minmax(60px, auto));
-  grid-template-columns: repeat(6, 60px);
-  grid-gap: 12px;
-  padding: 35px;
-  font-family: "Poppins";
-  font-weight: 300;
-  font-size: 18px;
-  background-color: #ffffff;
-  border-radius: 15px;
-  border: 2px solid #222;
-}
+  /*Use media query for rem*/
+  @media (min-width: 320px) {
+    html {
+      font-size: 14px;
+    }
+  }
 
-.btn,
-.zero {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  text-align: center;
-  text-decoration: none;
-  outline: none;
-  color: #484848;
-  background-color: lightgrey;
-  border-radius: 5px;
-  -webkit-box-shadow: -1px 8px 8px 1px rgba(0, 0, 0, 0.75);
-  -moz-box-shadow: -1px 8px 8px 1px rgba(0, 0, 0, 0.75);
-  box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.75);
-}
+  @media (min-width: 375px) {
+    html {
+      font-size: 16px;
+    }
+  }
 
-.display
-{
-  grid-column: 1 / 7;
-  display: flex;
-  align-items: center;
-  font-weight: 500;
-  color: #146080;
-  border-bottom: 2px solid #e1e1e1;
-  font-size: 55px;
-  height: 65px;
-  margin-bottom: 15px;
-  background-color: #EEE;
-}
+  @media (min-width: 440px) {
+    html {
+      font-size: 18px;
+    }
+  }
 
-.result {
-  font-weight: 500;
-  color: #146080;
-  font-size: 55px;
-  height: 65px;
-}
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #ccc;
+  }
 
-.zero {
-  grid-column: 2 / 4;
-}
+  .calculator {
+    background: #2E3A52;
+    flex: 1;
+    padding: 10px;
+    margin: 10px;
+    color: white;
+    border-radius: 10px;
+    box-shadow: 0 4px 6px 0.2px rgba(0, 0, 0, 0.6);
+    position: relative;
+    min-width: 280px;
+    max-width: 480px;
+  }
 
-.equal {
-  background-color: #0084ff !important;
-  color: white !important;
-}
+  .calculator-bar {
+    text-align: center;
+    font-size: 0.9rem;
+  }
 
-.operator {
-  background-color: rgb(255, 174, 0);
-  color: white;
-}
+  .calculator-display {
+    font-size: 2.5rem;
+    height: 100px;
+    padding: 0 10px;
+    background-color: #2E3A52;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    overflow: hidden;
+  }
 
-.basic {
-  background-color: grey;
-}
+  .calculator-keys {
+  }
+
+  .calculator-keys .row {
+    display: flex;
+    height: 60px;
+  }
+
+  .calculator-keys .row .calculator-btn {
+    cursor: pointer;
+    flex: 1;
+    padding: 5px;
+    margin: 5px;
+    height: 50px;
+    font-size: 1.3rem;
+    border: none;
+    background-color: #212C42;
+    border-radius: 7px;
+    transition: color .15s ease-in-out, background-color .15s ease-in-out, border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+    color: #19D5A3
+  }
+
+  .calculator-keys .row .calculator-btn.notshow {
+    visibility: hidden;
+  }
+
+  .calculator-keys .row .calculator-btn.eq-btn {
+    position: relative;
+    top: 0;
+    left: 0;
+    height: 110px;
+    background-color: #19D5A3;
+    color: #212C42
+  }
+
+  .calculator-keys .row .calculator-btn.num-btn {
+    color: #fff
+  }
 </style>
