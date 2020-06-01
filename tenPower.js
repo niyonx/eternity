36a8f4cb6cc7092ad20bfb6089ba01b95ad2d61c
Jenny <39646200+zengni2018@users.noreply.github.com>
2020-06-01 00:28:54 -0400

    Add files via upload (#14)

---
+++ tenPower.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line semi
+import {power} from './power';
+
+export function tenPower (num) {
+  // eslint-disable-next-line semi
+  let result = power(10, num);
+  return result
+}
