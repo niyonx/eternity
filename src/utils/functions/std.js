bd2678add3576f41dc02d8e857c12779dc513548
dadadei <49823349+dadadei@users.noreply.github.com>
2020-06-02 13:31:06 -0400

    std

---
+++ src/utils/functions/std.js
@@ -0,0 +1,20 @@
+export function std () {
+
+    //calculate the mean of inputs
+    var mean=0
+    for (var i = 0; i < arguments.length; i++) {
+      mean += arguments[i]
+    }
+    mean = mean/arguments.length
+
+    
+    var deviation_square=0 // init deviation_square (x-lambda)
+    var sum=0 //sum [(x-lambda)^2]
+    for (var i = 0; i < arguments.length; i++) {
+        deviation_square=pow(arguments[i]-mean, 2)
+        sum+=deviation_square
+    }
+
+    //divide the sum of difference by number of inputs
+    return sqrt(sum/arguments.length)
+  }
