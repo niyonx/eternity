04cf81e9a2a57f4db1bb38ae248e6992dae4386c
niyonx <kenkenysy@gmail.com>
2020-06-03 00:39:11 -0400

    Fix sin accuracy issue #13

--- src/utils/functions/sin.js
+++ src/utils/functions/sin.js
@@ -1,22 +1,21 @@
 /* eslint-disable */
 
 import {factorial} from './factorial';
-import {BigNumber} from 'bignumber.js';
 import {power} from './power';
 import {PI} from './PI';
 
-export function sin (num, angleMode, rounds = 40) {
+export function sin (num, angleMode, rounds = 100) {
   num = angleMode === 'deg' ? num * (PI / 180.0) : num;
-  let retained = new BigNumber(0);
-  let nominator = new BigNumber();
-  let denominator = new BigNumber();
-  let multiplier = new BigNumber();
+  let retained = 0;
+  let nominator = 0;
+  let denominator = 0;
+  let multiplier = 0;
 
   for (let i = 0; i < rounds; ++i) {
     nominator = power(-1, i);
     denominator = factorial(2 * i + 1);
     multiplier = power(num, 2 * i + 1)
-    retained = retained.plus(nominator.dividedBy(denominator).multipliedBy(multiplier));
+    retained = retained + (nominator / denominator * multiplier);
   }
 
   return retained;
