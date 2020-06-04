824e30ce19c9221bf582cc1b31ef6f07dba69375
yiyiyang81 <57012409+yiyiyang81@users.noreply.github.com>
2020-06-03 23:06:33 -0400

    Implementation of mad.js (#11)

--- src/utils/functions/mad.js
+++ src/utils/functions/mad.js
@@ -1,6 +1,24 @@
 
 export function mad () {
-  // @TODO: Write your code here and return the result
-  //  remember that the implicit variable "arguments" is the array the number used for calculation
-  return 0
-}
+
+    //calculate the mean of inputs
+    var mean=0
+    for (var i = 0; i < arguments.length; i++) {
+    	mean += arguments[i]
+    }
+  	mean = mean/arguments.length
+  
+    //sum up the difference between each number and mean
+    var deviation=0
+    var sum=0
+    for (var i = 0; i < arguments.length; i++) {
+        deviation=arguments[i]-mean
+        if (deviation<0){
+            deviation = -deviation
+        }
+        sum+=deviation
+    }
+
+    //divide the sum of difference by number of inputs
+    return sum/arguments.length
+  }
