30497b85dfa22158972f6262d761eb12f1c0bb15
niyonx <kenkenysy@gmail.com>
2020-06-04 10:55:56 -0400

    Fix sin accuracy + unified coding style

--- src/utils/functions/mad.js
+++ src/utils/functions/mad.js
@@ -1,24 +1,23 @@
 
 export function mad () {
-
-    //calculate the mean of inputs
-    var mean=0
-    for (var i = 0; i < arguments.length; i++) {
-    	mean += arguments[i]
-    }
-  	mean = mean/arguments.length
-  
-    //sum up the difference between each number and mean
-    var deviation=0
-    var sum=0
-    for (var i = 0; i < arguments.length; i++) {
-        deviation=arguments[i]-mean
-        if (deviation<0){
-            deviation = -deviation
-        }
-        sum+=deviation
-    }
-
-    //divide the sum of difference by number of inputs
-    return sum/arguments.length
+  // calculate the mean of inputs
+  let mean = 0
+  for (let i = 0; i < arguments.length; i++) {
+    mean += arguments[i]
   }
+  mean = mean / arguments.length
+
+  // sum up the difference between each number and mean
+  let deviation = 0
+  let sum = 0
+  for (let i = 0; i < arguments.length; i++) {
+    deviation = arguments[i] - mean
+    if (deviation < 0) {
+      deviation = -deviation
+    }
+    sum += deviation
+  }
+
+  // divide the sum of difference by number of inputs
+  return sum / arguments.length
+}
--- src/utils/functions/sin.js
+++ src/utils/functions/sin.js
@@ -1,9 +1,11 @@
 import {factorial} from './factorial'
 import {power} from './power'
 import {PI} from './PI'
+import {abs} from './abs'
 
 export function sin (num, angleMode, rounds = 100) {
   num = angleMode === 'deg' ? num * (PI / 180.0) : num
+  num = abs(num % (2 * PI))
   let retained = 0
   let nominator = 0
   let denominator = 0
