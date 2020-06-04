013ea220859f92a8d5e66acd69ec8660359ef03b
yiyiyang81 <57012409+yiyiyang81@users.noreply.github.com>
2020-06-04 10:52:28 -0400

    Update mad.js

--- src/utils/functions/mad.js
+++ src/utils/functions/mad.js
@@ -2,16 +2,16 @@
 export function mad () {
 
     //calculate the mean of inputs
-    var mean=0
-    for (var i = 0; i < arguments.length; i++) {
+    let mean=0
+    for (let i = 0; i < arguments.length; i++) {
     	mean += arguments[i]
     }
   	mean = mean/arguments.length
   
     //sum up the difference between each number and mean
-    var deviation=0
-    var sum=0
-    for (var i = 0; i < arguments.length; i++) {
+    let deviation=0
+    let sum=0
+    for (let i = 0; i < arguments.length; i++) {
         deviation=arguments[i]-mean
         if (deviation<0){
             deviation = -deviation
