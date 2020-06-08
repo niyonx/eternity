cffd3ae5bf003c244ab4051d142ec8aa4827a5bf
Yanxiao Yang <yangyanxiao@gmail.com>
2020-06-08 03:51:34 -0400

    Update power.js

--- src/utils/functions/power.js
+++ src/utils/functions/power.js
@@ -1,14 +1,33 @@
 import {abs} from './abs.js'
+import {ePower} from './ePower.js'
 
 export function power (num, exponent) {
   let result = 1
   let absExponent = abs(exponent)
-  for (let i = 1; i <= absExponent; ++i) {
-    result = result * num
+  if (absExponent % 1 !== 0) {
+    let decimalExponent = absExponent % 1
+    let wholeExponent = absExponent - decimalExponent
+    result = power(num, wholeExponent) * ePower(decimalExponent * myln(num))
+  } else {
+    for (let i = 1; i <= absExponent; ++i) {
+      result = result * num
+    }
   }
+
   if (exponent < 0) {
     return 1 / result
   } else {
     return result
   }
+}
+
+export function myln (num) {
+  let rounds = 300
+  let temp = ((num - 1) / (num + 1))
+  let result = 0
+  for (let i = 1; i < rounds * 2; i += 2) {
+    let j = (2.0 * i) - 1
+    result = result + (j * power(temp, j))
+  }
+  return (2.0 * result)
 }
