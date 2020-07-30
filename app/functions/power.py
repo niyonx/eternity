6d9a676bb5d731f7090d715a8983785838ecc5fe
Yanxiao Yang <yangyanxiao@gmail.com>
2020-07-30 16:19:39 -0400

    Revert "python env adjustment"
    This reverts commit 32f82c30eb5c300e47405ef717e921816262f392.

--- app/functions/power.py
+++ app/functions/power.py
@@ -1,30 +1,27 @@
 from .abs import abs
-from .ePower import ePower
 
-
-def power(num, exponent):
+def power (num, exponent):
+  from .ePower import ePower
   result = 1
-  abs_exponent = abs(exponent)
-  if abs_exponent % 1 != 0:
-    decimal_exponent = abs_exponent % 1
-    whole_exponent = int(abs_exponent)
-    result = power(num, whole_exponent) * ePower(decimal_exponent * my_ln(num))
+  absExponent = abs(exponent)
+  if (absExponent % 1 != 0):
+    decimalExponent = absExponent % 1
+    wholeExponent = absExponent - decimalExponent
+    result = power(num, wholeExponent) * ePower(decimalExponent * myln(num))
   else:
-    abs_exponent = int(abs_exponent)
-    for i in range(1, abs_exponent + 1):
+    for i in range(1,absExponent+1):
       result = result * num
 
-  if exponent < 0:
+  if (exponent < 0):
     return 1 / result
   else:
     return result
 
-
-def my_ln(num):
+def myln(num):
   rounds = 300
   temp = ((num - 1) / (num + 1))
   result = 0
-  for i in range(1, rounds * 2, 2):
+  for i in range(1,rounds*2,2):
     j = 1 / i
     result = result + (j * power(temp, i))
   return (2.0 * result)
