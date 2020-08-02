2b99e956140c6507336e67049b0e9c64af83ce85
niyonx <kenkenysy@gmail.com>
2020-08-02 10:46:01 -0400

    heroku change

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
+const IS_PRODUCTION = process.env.NODE_ENV === 'production'
+
 module.exports = {
+  outputDir: 'dist',
+  assetsDir: 'static',
+  baseUrl: IS_PRODUCTION
+  ? 'https://eternity-app.herokuapp.com'
+  : '/',
+  // For Production, replace set baseUrl to CDN
+  // And set the CDN origin to `yourdomain.com/static`
+  // Whitenoise will serve once to CDN which will then cache
+  // and distribute
   devServer: {
     proxy: {
       '/api*': {
@@ -6,8 +17,5 @@ module.exports = {
         target: 'http://localhost:5000/'
       }
     }
-  },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/eternity/'
-    : '/'
+  }
 }
